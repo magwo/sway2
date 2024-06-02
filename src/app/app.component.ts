d39c62cec7fb59dc8af516db1889efa7cd11f9cc
Magnus Wolffelt <magnus.wolffelt@gmail.com>
2024-06-02 10:37:37 +0200

    Basic page structure wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SceneComponent } from './scene/scene.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SceneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'appName';
+  title = 'Sway 2';
 }
---
+++ src/app/scene/scene.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sway-scene',
+  standalone: true,
+  imports: [],
+  templateUrl: './scene.component.html',
+  styleUrl: './scene.component.scss'
+})
+export class SceneComponent {
+
+}
